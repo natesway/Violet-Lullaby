f934469fe007021f9dc8ac8cc51a485f31c01b01
natesway <64743334+natesway@users.noreply.github.com>
2023-09-23 17:36:24 -0400

    Update GameOverSubstate.hx

--- source/meta/subState/GameOverSubstate.hx
+++ source/meta/subState/GameOverSubstate.hx
@@ -629,7 +629,7 @@ class GameOverSubstate extends MusicBeatSubState
 
 						bg.setGraphicSize(Std.int(FlxG.width), Std.int(FlxG.height));
 						bg.screenCenter();
-					}
+					})
 
 //					deathEnd = function()
 //					{
